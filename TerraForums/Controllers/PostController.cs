2d8e166f7410a72defe0dcff13d5c7f7989d512b
COGITO55 <134492933+COGITO55@users.noreply.github.com>
2023-07-05 20:49:01 +0200

    Add Post Controller, service methods

---
+++ TerraForums/Controllers/PostController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TerraForums.Data.Models;
+using TerraForums.Data;
+using TerraForums.Models.Post;
+using TerraForums.Models.Reply;
+
+namespace TerraForums.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly IPost _postService;
+        public PostController(IPost postService)
+        {
+            _postService = postService;
+        }
+
+        public IActionResult Index(int id)
+        {
+            var post = _postService.GetById(id);
+
+            var replies = BuildPostReplies(post.Replies);
+
+            var model = new PostIndexModel
+            {
+                Id = post.Id,
+                Title = post.Title,
+                AuthorId = post.User.Id,
+                AuthorName = post.User.UserName,
+                AuthorImageUrl = post.User.ProfileImageUrl,
+                Created = post.Created,
+                PostContent = post.Content,
+                Replies = replies
+            };
+
+            return View(model);
+        }
+
+        private IEnumerable<PostReplyModel> BuildPostReplies(IEnumerable<PostReply> replies)
+        {
+            return replies.Select(reply => new PostReplyModel
+            {
+                Id = reply.Id,
+                AuthorName = reply.User.UserName,
+                AuthorId = reply.User.Id,
+                AuthorImageUrl = reply.User.ProfileImageUrl,
+                Created = reply.Created,
+                ReplyContent = reply.Content
+            });
+        }
+    }
+}
